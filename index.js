aa4882f202a099858c06c8f39c49e3fa716646ff
abhishekmhatre10 <abhishekmhatre892001@gmail.com>
2024-04-23 19:32:54 -0400

    dcds

--- index.js
+++ index.js
@@ -2,40 +2,47 @@ var drumnumbers = document.querySelectorAll(".drum").length;
 for (var i = 0; i < drumnumbers; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
         var buttonselect = this.innerHTML;
-        switch (buttonselect) {
-            case "a":
-                var audio = new Audio("sounds/crash.mp3")
-                audio.play();
-                break;
-            case "b":
-                var audio = new Audio("sounds/kick-bass.mp3")
-                audio.play();    
-                break;
-            case "c":
-                var audio = new Audio("sounds/snare.mp3")
-                 audio.play();    
-                 break;
-            case "d":
-                var audio = new Audio("sounds/tom-1.mp3")
-                audio.play();    
-                break;
-            case "e":
-                var audio = new Audio("sounds/tom-2.mp3")
+        makeSound(buttonselect);
+        
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+})
+
+
+function makeSound(key) {
+    switch (key) {
+        case "a":
+            var audio = new Audio("sounds/crash.mp3")
+            audio.play();
+            break;
+        case "b":
+            var audio = new Audio("sounds/kick-bass.mp3")
             audio.play();    
             break;
-            case "f":
+        case "c":
+            var audio = new Audio("sounds/snare.mp3")
+             audio.play();    
+             break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3")
+            audio.play();    
+            break;
+        case "e":
+            var audio = new Audio("sounds/tom-2.mp3")
+            audio.play();    
+            break;
+        case "f":
             var audio = new Audio("sounds/tom-3.mp3")
             audio.play();    
             break;
-            case "g":
+        case "g":
             var audio = new Audio("sounds/tom-4.mp3")
             audio.play();    
             break;
-
-            default:
-                break;
-        }
-        
-        
-    });
+        default:
+            break;
+    }
 }
